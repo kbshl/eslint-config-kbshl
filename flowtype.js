e2538d92dd0a85520a71596bdab0a54b417d8b2a
Konstantin Büschel <konstantin.bueschel@gmail.com>
2019-01-30 13:19:29 +0100

    feat(flowtype): add flowtype config

---
+++ flowtype.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ['flowtype'],
+  rules: {
+    'flowtype/define-flow-type': 'warn',
+    'flowtype/require-valid-file-annotation': 'warn',
+    'flowtype/use-flow-type': 'warn',
+  },
+};
